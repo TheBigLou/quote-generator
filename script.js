c3e3e4bafc88d9d5ac5bcfcd6b7339fb4da99df6
Louis Hellman <louis.hellman@gmail.com>
2023-03-24 10:37:29 -0700

    hook up buttons

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
+// Create constants that will be populated with valaues below by grabbing the elements in the HTML file by their IDs
+const quoteContainer = document.getElementById('quote-constainer');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterButton = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new-quote');
+
 let apiQuotes = []; // use let because we'll change the value of this when we fetch the quotes below
 
-// Show new quote from API
+// Show new quote
 function newQuote() {
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    // Check for author and replace with 'Unknown' if needed
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // If quote is long, apply different CSS class to shrink it
+    if (quote.text.length > 120) {
+        quoteText.classList.add("long-quote"); // adds this class to the element
+    } else {
+        quoteText.classList.remove("long-quote"); // need to remove it for the next quote as appropriate
+    }
+    quoteText.textContent = quote.text;
 }
 
 // Show new quote from backup file in case API is down
 function newOfflineQuote() {
     const offQuote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    console.log(offQuote);
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
 }
 
-// Get quotes from API
+// Get quotes from API and store locally
 async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
     try {
@@ -25,6 +45,16 @@ async function getQuotes() {
         newOfflineQuote();
     }
 }
+
+// Tweet quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent}%0A-${authorText.textContent}`;
+    window.open(twitterUrl, '_blank'); // open twitter in a new tab with quote ready to tweet
+}
+
+// Event listeners
+newQuoteButton.addEventListener('click', newQuote);
+twitterButton.addEventListener('click', tweetQuote);
 
 // On page load, fetch quotes
 getQuotes();
